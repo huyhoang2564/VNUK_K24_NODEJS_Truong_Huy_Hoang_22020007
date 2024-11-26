e6ce431daa0e12416184634b21bdad000c3a19a8
huyhoang2564 <huyhoang2564@.com>
2024-11-26 10:41:30 +0700

    Huy Hoang sua bai

--- hw12/food_order/controllers/category.controller.js
+++ hw12/food_order/controllers/category.controller.js
@@ -1,29 +1,59 @@
 const categoryModel = require("../models/category.model");
 
-// CRUD: Create, Read, Update, Delete
-
-module.exports = {
-    createCategory: async (req, res) => {
-        const body = req.body;
-        const newCategory = await categoryModel.create(body);
-        return res.status(201).json(newCategory);
+const categoryController = {
+    getData: async (req, res) => {
+        try {
+            // Gọi API sử dụng fetch
+            const response = await fetch('http://localhost:3000/api/categories');
+            const data = await response.json();
+            
+            // Truyền dữ liệu vào view EJS
+            res.render('index', { data });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Error fetching data');
+        }
     },
 
-    getCategories: async (req, res)  => {
-        const categories = await categoryModel.find();
-        return res.status(200).json(categories);
+    createCategory: async (req, res) => {
+        const body = req.body;
+        try {
+            const newCategory = await categoryModel.create(body);
+            res.status(201).json(newCategory);
+        } catch (error) {
+            res.status(500).send('Error creating category');
+        }
+    },
+
+    getCategories: async (req, res) => {
+        try {
+            const categories = await categoryModel.find();
+            res.status(200).json(categories);
+        } catch (error) {
+            res.status(500).send('Error fetching categories');
+        }
     },
 
     updateCategory: async (req, res) => {
         const id = req.params.id;
         const body = req.body;
-        const updatedCategory = await categoryModel.findByIdAndUpdate(id, body, { new: true });
-        return res.status(200).json(updatedCategory);
+        try {
+            const updatedCategory = await categoryModel.findByIdAndUpdate(id, body, { new: true });
+            res.status(200).json(updatedCategory);
+        } catch (error) {
+            res.status(500).send('Error updating category');
+        }
     },
 
     deleteCategory: async (req, res) => {
         const id = req.params.id;
-        const deletedCategory = await categoryModel.findByIdAndDelete(id);
-        return res.status(200).json(deletedCategory);
+        try {
+            const deletedCategory = await categoryModel.findByIdAndDelete(id);
+            res.status(200).json(deletedCategory);
+        } catch (error) {
+            res.status(500).send('Error deleting category');
+        }
     }
 };
+
+module.exports = categoryController;
